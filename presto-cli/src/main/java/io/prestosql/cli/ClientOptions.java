fddd53ae1a68e281a00c8260828448a3845cb423
Vinitha Gankidi <vgankidi@netflix.com>
2018-12-06 18:22:56 -0800

    NETFLIX-BUILD: Allow genie_job_name session parameter to have spaces

--- presto-cli/src/main/java/io/prestosql/cli/ClientOptions.java
+++ presto-cli/src/main/java/io/prestosql/cli/ClientOptions.java
@@ -346,7 +346,9 @@ public class ClientOptions
             checkArgument(PRINTABLE_ASCII.matchesAllOf(catalog.orElse("")), "Session property catalog contains spaces or is not US_ASCII: %s", name);
             checkArgument(name.indexOf('=') < 0, "Session property name must not contain '=': %s", name);
             checkArgument(PRINTABLE_ASCII.matchesAllOf(name), "Session property name contains spaces or is not US_ASCII: %s", name);
-            checkArgument(PRINTABLE_ASCII.matchesAllOf(value), "Session property value contains spaces or is not US_ASCII: %s", value);
+            if (!name.equals("genie_job_name")) {
+                checkArgument(PRINTABLE_ASCII.matchesAllOf(value), "Session property value contains spaces or is not US_ASCII: %s", value);
+            }
         }
 
         public Optional<String> getCatalog()
