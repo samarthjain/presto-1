9668abe02f68d45d813d7220bfbc5641fe290c27
pbrahmbhatt <brahmbhatt.parth@gmail.com>
2019-10-24 14:40:37 -0700

    [Netflix-BUILD]: Presto-317 build script changes.

--- src/bin/jenkins_post_build.sh
+++ src/bin/jenkins_post_build.sh
@@ -4,7 +4,7 @@ set -x
 set -o errexit -o nounset -o pipefail
 
 export NETFLIX_ENVIRONMENT=prod
-PRESTO_VERSION=`ls presto-server/target/*.tar.gz  | xargs -n 1 basename | cut -d'-' -f3|cut -d'.' -f1,2`
+PRESTO_VERSION=`ls presto-server/target/*.tar.gz  | xargs -n 1 basename | cut -d'-' -f3| cut -d'.' -f1`
 CLI_BUILD_PATH=s3://netflix-bigdataplatform/presto/clients/${PRESTO_VERSION}/${BUILD_NUMBER_FORMATTED}/presto-cli
 echo "Pushing presto-cli to $CLI_BUILD_PATH"
 /apps/python/bin/python /usr/local/bin/assume-role -a arn:aws:iam::219382154434:role/BDP_JENKINS_ROLE -r us-east-1 aws s3 cp presto-cli/target/presto-cli*executable*.jar $CLI_BUILD_PATH
