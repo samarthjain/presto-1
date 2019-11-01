trinodb/trino

Implement local dynamic filtering for broadcast inner-joins
When a broadcast inner-join is used, probe-side table scan and hash build
operators are running in the same process - so the dynamic filter can be
extracted and applied locally, without involving the coordinator.

The connector may optimize the probe-side scan significantly by applying
the dynamic filter during page source creation using run-time predicate pushdown.

Also, add dynamic filtering support for MemoryPageSourceProvider and add a few simple smoke tests.

For more details, see https://www.slideshare.net/OriReshef/dynamic-filtering-for-presto-join-optimisation

presto-main/src/main/java/io/prestosql/SystemSessionProperties.java
@@ -121,6 +121,8 @@ public final class SystemSessionProperties
     public static final String ENABLE_DYNAMIC_FILTERING = "enable_dynamic_filtering";
     public static final String QUERY_MAX_MEMORY_PER_NODE = "query_max_memory_per_node";
     public static final String QUERY_MAX_TOTAL_MEMORY_PER_NODE = "query_max_total_memory_per_node";
+    public static final String DYNAMIC_FILTERING_MAX_PER_DRIVER_VALUE_COUNT = "dynamic_filtering_max_per_driver_value_count";
+    public static final String DYNAMIC_FILTERING_MAX_PER_DRIVER_SIZE = "dynamic_filtering_max_per_driver_size";
 
     private final List<PropertyMetadata<?>> sessionProperties;
 
@@ -523,7 +525,17 @@ public final class SystemSessionProperties
                         QUERY_MAX_TOTAL_MEMORY_PER_NODE,
                         "Maximum amount of total memory a query can use per node",
                         nodeMemoryConfig.getMaxQueryTotalMemoryPerNode(),
-                        true));
+                        true),
+                integerProperty(
+                        DYNAMIC_FILTERING_MAX_PER_DRIVER_VALUE_COUNT,
+                        "Experimental: maximum number of build-side rows to be collected for dynamic filtering per-driver",
+                        featuresConfig.getDynamicFilteringMaxPerDriverRowCount(),
+                        false),
+                dataSizeProperty(
+                        DYNAMIC_FILTERING_MAX_PER_DRIVER_SIZE,
+                        "Experimental: maximum number of bytes to be collected for dynamic filtering per-driver",
+                        featuresConfig.getDynamicFilteringMaxPerDriverSize(),
+                        false));
     }
 
     public List<PropertyMetadata<?>> getSessionProperties()
@@ -929,5 +941,15 @@ public final class SystemSessionProperties
     public static DataSize getQueryMaxTotalMemoryPerNode(Session session)
     {
         return session.getSystemProperty(QUERY_MAX_TOTAL_MEMORY_PER_NODE, DataSize.class);
+    }
+
+    public static int getDynamicFilteringMaxPerDriverValueCount(Session session)
+    {
+        return session.getSystemProperty(DYNAMIC_FILTERING_MAX_PER_DRIVER_VALUE_COUNT, Integer.class);
+    }
+
+    public static DataSize getDynamicFilteringMaxPerDriverSize(Session session)
+    {
+        return session.getSystemProperty(DYNAMIC_FILTERING_MAX_PER_DRIVER_SIZE, DataSize.class);
     }
 }

presto-main/src/test/java/io/prestosql/operator/BenchmarkDynamicFilterSourceOperator.java
@@ -1,0 +1,194 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.prestosql.operator;
+
+import com.google.common.collect.ImmutableList;
+import io.airlift.tpch.LineItem;
+import io.airlift.tpch.LineItemGenerator;
+import io.airlift.units.DataSize;
+import io.prestosql.spi.Page;
+import io.prestosql.spi.PageBuilder;
+import io.prestosql.sql.planner.plan.PlanNodeId;
+import io.prestosql.testing.TestingTaskContext;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.TearDown;
+import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+import org.openjdk.jmh.runner.options.VerboseMode;
+import org.testng.annotations.Test;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import static io.airlift.concurrent.Threads.daemonThreadsNamed;
+import static io.airlift.units.DataSize.Unit.GIGABYTE;
+import static io.prestosql.SessionTestUtils.TEST_SESSION;
+import static io.prestosql.SystemSessionProperties.getDynamicFilteringMaxPerDriverSize;
+import static io.prestosql.SystemSessionProperties.getDynamicFilteringMaxPerDriverValueCount;
+import static io.prestosql.spi.type.BigintType.BIGINT;
+import static java.util.concurrent.Executors.newCachedThreadPool;
+import static java.util.concurrent.Executors.newScheduledThreadPool;
+import static org.testng.Assert.assertEquals;
+
+@State(Scope.Thread)
+@OutputTimeUnit(TimeUnit.SECONDS)
+@Fork(3)
+@Warmup(iterations = 20, time = 500, timeUnit = TimeUnit.MILLISECONDS)
+@Measurement(iterations = 20, time = 500, timeUnit = TimeUnit.MILLISECONDS)
+public class BenchmarkDynamicFilterSourceOperator
+{
+    private static final int TOTAL_POSITIONS = 1_000_000;
+
+    @State(Scope.Thread)
+    public static class BenchmarkContext
+    {
+        @Param({"32", "256", "1024"})
+        private String positionsPerPage = "32";
+
+        private ExecutorService executor;
+        private ScheduledExecutorService scheduledExecutor;
+        private OperatorFactory operatorFactory;
+        private List<Page> pages;
+
+        @Setup
+        public void setup()
+        {
+            executor = newCachedThreadPool(daemonThreadsNamed("test-executor-%s"));
+            scheduledExecutor = newScheduledThreadPool(2, daemonThreadsNamed("test-scheduledExecutor-%s"));
+
+            pages = createInputPages(Integer.valueOf(positionsPerPage));
+
+            operatorFactory = new DynamicFilterSourceOperator.DynamicFilterSourceOperatorFactory(
+                    1,
+                    new PlanNodeId("joinNodeId"),
+                    (tupleDomain -> {}),
+                    ImmutableList.of(new DynamicFilterSourceOperator.Channel("0", BIGINT, 0)),
+                    getDynamicFilteringMaxPerDriverValueCount(TEST_SESSION),
+                    getDynamicFilteringMaxPerDriverSize(TEST_SESSION));
+        }
+
+        @TearDown
+        public void cleanup()
+        {
+            executor.shutdownNow();
+            scheduledExecutor.shutdownNow();
+        }
+
+        public TaskContext createTaskContext()
+        {
+            return TestingTaskContext.createTaskContext(executor, scheduledExecutor, TEST_SESSION, new DataSize(2, GIGABYTE));
+        }
+
+        public OperatorFactory getOperatorFactory()
+        {
+            return operatorFactory;
+        }
+
+        public List<Page> getPages()
+        {
+            return pages;
+        }
+
+        private static List<Page> createInputPages(int positionsPerPage)
+        {
+            ImmutableList.Builder<Page> pages = ImmutableList.builder();
+            PageBuilder pageBuilder = new PageBuilder(ImmutableList.of(BIGINT));
+            LineItemGenerator lineItemGenerator = new LineItemGenerator(1, 1, 1);
+            Iterator<LineItem> iterator = lineItemGenerator.iterator();
+            for (int i = 0; i < TOTAL_POSITIONS; i++) {
+                pageBuilder.declarePosition();
+
+                LineItem lineItem = iterator.next();
+                BIGINT.writeLong(pageBuilder.getBlockBuilder(0), lineItem.getOrderKey());
+
+                if (pageBuilder.getPositionCount() == positionsPerPage) {
+                    pages.add(pageBuilder.build());
+                    pageBuilder.reset();
+                }
+            }
+
+            if (pageBuilder.getPositionCount() > 0) {
+                pages.add(pageBuilder.build());
+            }
+            return pages.build();
+        }
+    }
+
+    @Benchmark
+    public List<Page> dynamicFilterCollect(BenchmarkContext context)
+    {
+        DriverContext driverContext = context.createTaskContext().addPipelineContext(0, true, true, false).addDriverContext();
+        Operator operator = context.getOperatorFactory().createOperator(driverContext);
+
+        Iterator<Page> input = context.getPages().iterator();
+        ImmutableList.Builder<Page> outputPages = ImmutableList.builder();
+
+        boolean finishing = false;
+        for (int loops = 0; !operator.isFinished() && loops < 1_000_000; loops++) {
+            if (operator.needsInput()) {
+                if (input.hasNext()) {
+                    Page inputPage = input.next();
+                    operator.addInput(inputPage);
+                }
+                else if (!finishing) {
+                    operator.finish();
+                    finishing = true;
+                }
+            }
+
+            Page outputPage = operator.getOutput();
+            if (outputPage != null) {
+                outputPages.add(outputPage);
+            }
+        }
+
+        return outputPages.build();
+    }
+
+    @Test
+    public void testBenchmark()
+    {
+        BenchmarkContext context = new BenchmarkContext();
+        context.setup();
+
+        List<Page> outputPages = dynamicFilterCollect(context);
+        assertEquals(TOTAL_POSITIONS, outputPages.stream().mapToInt(Page::getPositionCount).sum());
+
+        context.cleanup();
+    }
+
+    public static void main(String[] args)
+            throws RunnerException
+    {
+        Options options = new OptionsBuilder()
+                .verbosity(VerboseMode.NORMAL)
+                .include(".*" + BenchmarkDynamicFilterSourceOperator.class.getSimpleName() + ".*")
+                .build();
+
+        new Runner(options).run();
+    }
+}

samarthjain/presto-1

presto-main/src/main/java/io/prestosql/SystemSessionProperties.java
@@ -131,6 +131,8 @@ public final class SystemSessionProperties
     public static final String ICEBERG_HIVE_MAPPING = "iceberg_hive_mapping";
     public static final String QUERY_MAX_MEMORY_PER_NODE = "query_max_memory_per_node";
     public static final String QUERY_MAX_TOTAL_MEMORY_PER_NODE = "query_max_total_memory_per_node";
+    public static final String DYNAMIC_FILTERING_MAX_PER_DRIVER_ROW_COUNT = "dynamic_filtering_max_per_driver_row_count";
+    public static final String DYNAMIC_FILTERING_MAX_PER_DRIVER_SIZE = "dynamic_filtering_max_per_driver_size";
 
     private final List<PropertyMetadata<?>> sessionProperties;
 
@@ -592,7 +594,17 @@ public final class SystemSessionProperties
                         QUERY_MAX_TOTAL_MEMORY_PER_NODE,
                         "Maximum amount of total memory a query can use per node",
                         nodeMemoryConfig.getMaxQueryTotalMemoryPerNode(),
-                        true));
+                        true),
+                integerProperty(
+                        DYNAMIC_FILTERING_MAX_PER_DRIVER_ROW_COUNT,
+                        "Experimental: maximum number of build-side rows to be collected for dynamic filtering per-driver",
+                        featuresConfig.getDynamicFilteringMaxPerDriverRowCount(),
+                        false),
+                dataSizeProperty(
+                        DYNAMIC_FILTERING_MAX_PER_DRIVER_SIZE,
+                        "Experimental: maximum number of bytes to be collected for dynamic filtering per-driver",
+                        featuresConfig.getDynamicFilteringMaxPerDriverSize(),
+                        false));
     }
 
     public List<PropertyMetadata<?>> getSessionProperties()
@@ -1027,5 +1039,15 @@ public final class SystemSessionProperties
     public static DataSize getQueryMaxTotalMemoryPerNode(Session session)
     {
         return session.getSystemProperty(QUERY_MAX_TOTAL_MEMORY_PER_NODE, DataSize.class);
+    }
+
+    public static int getDynamicFilteringMaxPerDriverRowCount(Session session)
+    {
+        return session.getSystemProperty(DYNAMIC_FILTERING_MAX_PER_DRIVER_ROW_COUNT, Integer.class);
+    }
+
+    public static DataSize getDynamicFilteringMaxPerDriverSize(Session session)
+    {
+        return session.getSystemProperty(DYNAMIC_FILTERING_MAX_PER_DRIVER_SIZE, DataSize.class);
     }
 }

presto-main/src/test/java/io/prestosql/operator/BenchmarkDynamicFilterSourceOperator.java
@@ -1,0 +1,194 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.prestosql.operator;
+
+import com.google.common.collect.ImmutableList;
+import io.airlift.tpch.LineItem;
+import io.airlift.tpch.LineItemGenerator;
+import io.airlift.units.DataSize;
+import io.prestosql.spi.Page;
+import io.prestosql.spi.PageBuilder;
+import io.prestosql.sql.planner.plan.PlanNodeId;
+import io.prestosql.testing.TestingTaskContext;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.TearDown;
+import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+import org.openjdk.jmh.runner.options.VerboseMode;
+import org.testng.annotations.Test;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import static io.airlift.concurrent.Threads.daemonThreadsNamed;
+import static io.airlift.units.DataSize.Unit.GIGABYTE;
+import static io.prestosql.SessionTestUtils.TEST_SESSION;
+import static io.prestosql.SystemSessionProperties.getDynamicFilteringMaxPerDriverRowCount;
+import static io.prestosql.SystemSessionProperties.getDynamicFilteringMaxPerDriverSize;
+import static io.prestosql.spi.type.BigintType.BIGINT;
+import static java.util.concurrent.Executors.newCachedThreadPool;
+import static java.util.concurrent.Executors.newScheduledThreadPool;
+import static org.testng.Assert.assertEquals;
+
+@State(Scope.Thread)
+@OutputTimeUnit(TimeUnit.SECONDS)
+@Fork(3)
+@Warmup(iterations = 20, time = 500, timeUnit = TimeUnit.MILLISECONDS)
+@Measurement(iterations = 20, time = 500, timeUnit = TimeUnit.MILLISECONDS)
+public class BenchmarkDynamicFilterSourceOperator
+{
+    private static final int TOTAL_POSITIONS = 1_000_000;
+
+    @State(Scope.Thread)
+    public static class BenchmarkContext
+    {
+        @Param({"32", "256", "1024"})
+        private String positionsPerPage = "32";
+
+        private ExecutorService executor;
+        private ScheduledExecutorService scheduledExecutor;
+        private OperatorFactory operatorFactory;
+        private List<Page> pages;
+
+        @Setup
+        public void setup()
+        {
+            executor = newCachedThreadPool(daemonThreadsNamed("test-executor-%s"));
+            scheduledExecutor = newScheduledThreadPool(2, daemonThreadsNamed("test-scheduledExecutor-%s"));
+
+            pages = createInputPages(Integer.valueOf(positionsPerPage));
+
+            operatorFactory = new DynamicFilterSourceOperator.DynamicFilterSourceOperatorFactory(
+                    1,
+                    new PlanNodeId("joinNodeId"),
+                    (tupleDomain -> {}),
+                    ImmutableList.of(new DynamicFilterSourceOperator.Channel("0", BIGINT, 0)),
+                    getDynamicFilteringMaxPerDriverRowCount(TEST_SESSION),
+                    getDynamicFilteringMaxPerDriverSize(TEST_SESSION));
+        }
+
+        @TearDown
+        public void cleanup()
+        {
+            executor.shutdownNow();
+            scheduledExecutor.shutdownNow();
+        }
+
+        public TaskContext createTaskContext()
+        {
+            return TestingTaskContext.createTaskContext(executor, scheduledExecutor, TEST_SESSION, new DataSize(2, GIGABYTE));
+        }
+
+        public OperatorFactory getOperatorFactory()
+        {
+            return operatorFactory;
+        }
+
+        public List<Page> getPages()
+        {
+            return pages;
+        }
+
+        private static List<Page> createInputPages(int positionsPerPage)
+        {
+            ImmutableList.Builder<Page> pages = ImmutableList.builder();
+            PageBuilder pageBuilder = new PageBuilder(ImmutableList.of(BIGINT));
+            LineItemGenerator lineItemGenerator = new LineItemGenerator(1, 1, 1);
+            Iterator<LineItem> iterator = lineItemGenerator.iterator();
+            for (int i = 0; i < TOTAL_POSITIONS; i++) {
+                pageBuilder.declarePosition();
+
+                LineItem lineItem = iterator.next();
+                BIGINT.writeLong(pageBuilder.getBlockBuilder(0), lineItem.getOrderKey());
+
+                if (pageBuilder.getPositionCount() == positionsPerPage) {
+                    pages.add(pageBuilder.build());
+                    pageBuilder.reset();
+                }
+            }
+
+            if (pageBuilder.getPositionCount() > 0) {
+                pages.add(pageBuilder.build());
+            }
+            return pages.build();
+        }
+    }
+
+    @Benchmark
+    public List<Page> dynamicFilterCollect(BenchmarkContext context)
+    {
+        DriverContext driverContext = context.createTaskContext().addPipelineContext(0, true, true, false).addDriverContext();
+        Operator operator = context.getOperatorFactory().createOperator(driverContext);
+
+        Iterator<Page> input = context.getPages().iterator();
+        ImmutableList.Builder<Page> outputPages = ImmutableList.builder();
+
+        boolean finishing = false;
+        for (int loops = 0; !operator.isFinished() && loops < 1_000_000; loops++) {
+            if (operator.needsInput()) {
+                if (input.hasNext()) {
+                    Page inputPage = input.next();
+                    operator.addInput(inputPage);
+                }
+                else if (!finishing) {
+                    operator.finish();
+                    finishing = true;
+                }
+            }
+
+            Page outputPage = operator.getOutput();
+            if (outputPage != null) {
+                outputPages.add(outputPage);
+            }
+        }
+
+        return outputPages.build();
+    }
+
+    @Test
+    public void testBenchmark()
+    {
+        BenchmarkContext context = new BenchmarkContext();
+        context.setup();
+
+        List<Page> outputPages = dynamicFilterCollect(context);
+        assertEquals(TOTAL_POSITIONS, outputPages.stream().mapToInt(Page::getPositionCount).sum());
+
+        context.cleanup();
+    }
+
+    public static void main(String[] args)
+            throws RunnerException
+    {
+        Options options = new OptionsBuilder()
+                .verbosity(VerboseMode.NORMAL)
+                .include(".*" + BenchmarkDynamicFilterSourceOperator.class.getSimpleName() + ".*")
+                .build();
+
+        new Runner(options).run();
+    }
+}