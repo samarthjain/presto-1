Parth-Brahmbhatt/presto

[NETFLIX-BUILD]: Netflix access control
(cherry picked from commit f9dbbd1cedb5d41e0123ba6e79daedc9cee8d43e)

presto-hive/src/main/java/com/facebook/presto/hive/netflix/NetflixSecurityModule.java
@@ -1,0 +1,29 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.hive.netflix;
+
+import com.facebook.presto.spi.connector.ConnectorAccessControl;
+import com.google.inject.Binder;
+import com.google.inject.Module;
+import com.google.inject.Scopes;
+
+public class NetflixSecurityModule
+        implements Module
+{
+    @Override
+    public void configure(Binder binder)
+    {
+        binder.bind(ConnectorAccessControl.class).to(NetflixAccessControl.class).in(Scopes.SINGLETON);
+    }
+}

presto-main/src/main/java/com/facebook/presto/metadata/SessionPropertyManager.java
@@ -177,10 +177,9 @@ public final class SessionPropertyManager
     public <T> T decodeCatalogPropertyValue(ConnectorId connectorId, String catalogName, String propertyName, @Nullable String propertyValue, Class<T> type)
     {
         String fullPropertyName = catalogName + "." + propertyName;
-        PropertyMetadata<?> property = getConnectorSessionPropertyMetadata(connectorId, propertyName)
+        PropertyMetadata<?> property = getConnectorSessionPropertyMetadata(connectorId, transformPropertyName(propertyName))
                 .orElseThrow(() -> new PrestoException(INVALID_SESSION_PROPERTY, "Unknown session property " + fullPropertyName));
-
-        return decodePropertyValue(fullPropertyName, propertyValue, type, property);
+        return decodePropertyValue(transformPropertyName(fullPropertyName), propertyValue, type, property);
     }
 
     public void validateSystemSessionProperty(String propertyName, String propertyValue)
@@ -194,10 +193,10 @@ public final class SessionPropertyManager
     public void validateCatalogSessionProperty(ConnectorId connectorId, String catalogName, String propertyName, String propertyValue)
     {
         String fullPropertyName = catalogName + "." + propertyName;
-        PropertyMetadata<?> propertyMetadata = getConnectorSessionPropertyMetadata(connectorId, propertyName)
+        PropertyMetadata<?> propertyMetadata = getConnectorSessionPropertyMetadata(connectorId, transformPropertyName(propertyName))
                 .orElseThrow(() -> new PrestoException(INVALID_SESSION_PROPERTY, "Unknown session property " + fullPropertyName));
 
-        decodePropertyValue(fullPropertyName, propertyValue, propertyMetadata.getJavaType(), propertyMetadata);
+        decodePropertyValue(transformPropertyName(fullPropertyName), propertyValue, propertyMetadata.getJavaType(), propertyMetadata);
     }
 
     private static <T> T decodePropertyValue(String fullPropertyName, @Nullable String propertyValue, Class<T> type, PropertyMetadata<?> metadata)
@@ -338,6 +337,21 @@ public final class SessionPropertyManager
             return Double.class;
         }
         throw new PrestoException(INVALID_SESSION_PROPERTY, format("Session property map key type %s is not supported", type));
+    }
+
+    private String transformPropertyName(String name)
+    {
+        // HACKING netflix specific session property aws_iam_role_<schema>
+        // if we have catalog.aws_iam_role, we just get the property.
+        // if we get catalog.aws_iam_role_<schema>, we return catalog.aws_iam_role.
+        // if we get only the session property name without the catalog, we just return the aws_iam_role.
+        final String awsIamRole = "aws_iam_role";
+        if (name.contains(awsIamRole) && !name.endsWith(awsIamRole)) {
+            // This would fail if we ever have schema with '.'
+            return name.contains(".") ? name.substring(0, name.indexOf('.')) + "." + awsIamRole : awsIamRole;
+        }
+
+        return name;
     }
 
     public static class SessionPropertyValue

samarthjain/presto-1

presto-hive/src/main/java/io/prestosql/plugin/hive/netflix/NetflixSecurityModule.java
@@ -1,0 +1,29 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.prestosql.plugin.hive.netflix;
+
+import com.google.inject.Binder;
+import com.google.inject.Module;
+import com.google.inject.Scopes;
+import io.prestosql.spi.connector.ConnectorAccessControl;
+
+public class NetflixSecurityModule
+        implements Module
+{
+    @Override
+    public void configure(Binder binder)
+    {
+        binder.bind(ConnectorAccessControl.class).to(NetflixAccessControl.class).in(Scopes.SINGLETON);
+    }
+}

presto-main/src/main/java/io/prestosql/metadata/SessionPropertyManager.java
@@ -177,10 +177,9 @@ public final class SessionPropertyManager
     public <T> T decodeCatalogPropertyValue(CatalogName catalog, String catalogName, String propertyName, @Nullable String propertyValue, Class<T> type)
     {
         String fullPropertyName = catalogName + "." + propertyName;
-        PropertyMetadata<?> property = getConnectorSessionPropertyMetadata(catalog, propertyName)
+        PropertyMetadata<?> property = getConnectorSessionPropertyMetadata(catalog, transformPropertyName(propertyName))
                 .orElseThrow(() -> new PrestoException(INVALID_SESSION_PROPERTY, "Unknown session property " + fullPropertyName));
-
-        return decodePropertyValue(fullPropertyName, propertyValue, type, property);
+        return decodePropertyValue(transformPropertyName(fullPropertyName), propertyValue, type, property);
     }
 
     public void validateSystemSessionProperty(String propertyName, String propertyValue)
@@ -194,10 +193,10 @@ public final class SessionPropertyManager
     public void validateCatalogSessionProperty(CatalogName catalog, String catalogName, String propertyName, String propertyValue)
     {
         String fullPropertyName = catalogName + "." + propertyName;
-        PropertyMetadata<?> propertyMetadata = getConnectorSessionPropertyMetadata(catalog, propertyName)
+        PropertyMetadata<?> propertyMetadata = getConnectorSessionPropertyMetadata(catalog, transformPropertyName(propertyName))
                 .orElseThrow(() -> new PrestoException(INVALID_SESSION_PROPERTY, "Unknown session property " + fullPropertyName));
 
-        decodePropertyValue(fullPropertyName, propertyValue, propertyMetadata.getJavaType(), propertyMetadata);
+        decodePropertyValue(transformPropertyName(fullPropertyName), propertyValue, propertyMetadata.getJavaType(), propertyMetadata);
     }
 
     private static <T> T decodePropertyValue(String fullPropertyName, @Nullable String propertyValue, Class<T> type, PropertyMetadata<?> metadata)
@@ -338,6 +337,21 @@ public final class SessionPropertyManager
             return Double.class;
         }
         throw new PrestoException(INVALID_SESSION_PROPERTY, format("Session property map key type %s is not supported", type));
+    }
+
+    private String transformPropertyName(String name)
+    {
+        // HACKING netflix specific session property aws_iam_role_<schema>
+        // if we have catalog.aws_iam_role, we just get the property.
+        // if we get catalog.aws_iam_role_<schema>, we return catalog.aws_iam_role.
+        // if we get only the session property name without the catalog, we just return the aws_iam_role.
+        final String awsIamRole = "aws_iam_role";
+        if (name.contains(awsIamRole) && !name.endsWith(awsIamRole)) {
+            // This would fail if we ever have schema with '.'
+            return name.contains(".") ? name.substring(0, name.indexOf('.')) + "." + awsIamRole : awsIamRole;
+        }
+
+        return name;
     }
 
     public static class SessionPropertyValue