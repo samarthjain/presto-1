Parth-Brahmbhatt/presto

[NETFLIX-BUILD]: Add a config to queue queries when the number of active workers is insufficient
(cherry picked from commit 8c7a30a3e22b21ae517d8b197d1d7ec7fec5d73b)

presto-main/src/test/java/com/facebook/presto/execution/resourceGroups/BenchmarkResourceGroup.java
@@ -83,7 +83,7 @@ public class BenchmarkResourceGroup
                 group.setHardConcurrencyLimit(queries);
             }
             for (int i = 0; i < queries; i++) {
-                group.run(new MockQueryExecution(10));
+                group.run(new MockQueryExecution(10), false);
             }
         }
 

samarthjain/presto-1

presto-main/src/test/java/io/prestosql/execution/resourcegroups/BenchmarkResourceGroup.java
@@ -83,7 +83,7 @@ public class BenchmarkResourceGroup
                 group.setHardConcurrencyLimit(queries);
             }
             for (int i = 0; i < queries; i++) {
-                group.run(new MockManagedQueryExecution(10));
+                group.run(new MockManagedQueryExecution(10), false);
             }
         }
 